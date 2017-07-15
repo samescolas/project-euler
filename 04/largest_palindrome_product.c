3bccf72bef50c1670842d282a2e08016fcb0bc3f
Sam Escolas <sescolas@student.42.us.org>
2017-07-15 14:37:01 -0400

    Largest palindrome function built

--- 04/largest_palindrome_product.c
+++ 04/largest_palindrome_product.c
@@ -2,25 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-int		is_palindrome(char *num);
+int		is_palindrome(long n);
+long	largest_palindrome(long  upper_bound);
 
 int		main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		if (is_palindrome(av[1]))
+		if (is_palindrome(atoll(av[1])))
 			printf("%s is a palindrome!\n", av[1]);
 		else
-			printf("%s is not a palindrome!\n", av[1]);
+			printf("%lu is the largest palindrome!\n", largest_palindrome(atoll(av[1])));
 	}
 	return (0);
 }
 
-int		is_palindrome(char *num)
+long	find_palindrome(int product_digits)
+{
+	
+}
+
+int		is_palindrome(long n)
 {
 	int		i;
 	int		len;
+	char	num[24];
 
+	sprintf(num, "%lu", n);
 	len = strlen(num);
 	i = -1;
 	while (num[++i] != '\0')
@@ -29,4 +37,18 @@ int		is_palindrome(char *num)
 			return (0);
 	}
 	return (1);
+}
+
+long	largest_palindrome(long upper_bound)
+{
+	long search_val;
+
+	search_val = upper_bound - 1;
+	while (1)
+	{
+		if (is_palindrome(search_val))
+			return (search_val);
+		search_val -= 1;
+	}
+	return (-1);
 }
