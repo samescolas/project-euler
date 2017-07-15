03d42365803f37a3bb091abf77cf1191e296f45c
Sam Escolas <sescolas@student.42.us.org>
2017-07-15 14:56:51 -0400

    Working!

--- 04/largest_palindrome_product.c
+++ 04/largest_palindrome_product.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int		is_palindrome(long n);
 long	largest_palindrome(long  upper_bound);
+long	find_palindrome(int product_digits);
 
 int		main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		if (is_palindrome(atoll(av[1])))
-			printf("%s is a palindrome!\n", av[1]);
-		else
-			printf("%lu is the largest palindrome!\n", largest_palindrome(atoll(av[1])));
+		find_palindrome(atoi(av[1]));
 	}
 	return (0);
 }
 
 long	find_palindrome(int product_digits)
 {
-	
+	long	lower_bound;
+	long	upper_bound;
+	long	search_val;
+	long	f1;
+	int		palindrome_found;
+
+	lower_bound = pow(10, product_digits - 1);
+	upper_bound = pow(10, product_digits) - 1;
+
+	search_val = largest_palindrome(pow(upper_bound, 2) + 1);
+	palindrome_found = 0;
+	while (!palindrome_found)
+	{
+		if (is_palindrome(search_val))
+		{
+			for (long factor=upper_bound; factor >= lower_bound && search_val / factor > lower_bound; factor--)
+			{
+				if (search_val % factor == 0 && search_val / factor >= lower_bound && search_val / factor <= upper_bound)
+				{
+					palindrome_found = 1;
+					f1 = factor;
+					break ;
+				}
+
+			}
+		}
+		if (!palindrome_found)
+			search_val = largest_palindrome(search_val);
+	}
+	printf("%lu is the product of %lu and %lu!\n", search_val, f1, search_val / f1);
 }
 
 int		is_palindrome(long n)
