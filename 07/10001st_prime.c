6c97aa0e2cc9e2ddd49d1b55eb90bac0f0025932
Sam Escolas <sescolas@student.42.us.org>
2017-07-22 18:19:14 -0400

    Cool cool

--- 07/10001st_prime.c
+++ 07/10001st_prime.c
@@ -2,18 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 
-int		is_prime(long long n);
+int			is_prime(long long n);
+long long	get_prime(int nth);
 
 int		main(int ac, char **av) 
 {
+	int		bound;
+
 	if (ac == 2)
-	{
-		if (is_prime(atoll(av[1])))
-			printf("%lld is prime!\n", atoll(av[1]));
-		else
-			printf("%lld isn't prime!\n", atoll(av[1]));
-	}
+		printf("%lld\n", get_prime(atoi(av[1])));
 	return (0);
+}
+
+long long	get_prime(int nth)
+{
+	long long	prime = 2;
+	int			count = 0;
+
+	while (count < nth)
+	{
+		if (is_prime(prime))
+			++count;
+		if (count == nth)
+			return (prime);
+		++prime;
+	}
 }
 
 int		is_prime(long long n)
