caba9f0e03f2a76c6742cbd5c295684d918a2ad2
Sam Escolas <samescolas@gmail.com>
2017-12-06 19:38:14 -0500

    Adding comments.

--- 50/consecutive_primes.py
+++ 50/consecutive_primes.py
@@ -2,6 +2,7 @@
 
 from math import sqrt
 
+# We're searching for the smallest number < 1,000,000
 MAX_VAL = 1_000_000
 
 def is_prime(n):
@@ -13,6 +14,8 @@ def is_prime(n):
 			return False
 	return True
 
+# find the max number of primes starting with prime[ix] (searching through all indices below) s.t. the
+# sum of all consecutive prime values from that one is prime and < MAX_VAL (1,000,000 for ProjectEuler)
 def find_max_sum(ix, primes):
 	if primes[ix] > MAX_VAL:
 		return 0,0
@@ -22,11 +25,9 @@ def find_max_sum(ix, primes):
 	prime_candidates = list(filter(lambda n: n < MAX_VAL and is_prime(n), candidates))
 	return candidates.index(prime_candidates[-1]) + 1, prime_candidates[-1]
 
-
+# generate a list of primes
 primes = list(filter(lambda x: is_prime(x), range(1, 100_000)))
-prime_prime_sums = []
-max_len = 0
-max_val = 0
+max_len, max_val = 0, 0
 
 for start_ix in range(250):
 	tmp_max_len, tmp_max_val = find_max_sum(start_ix, primes)
