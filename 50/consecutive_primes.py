7899ebcfa66becc7af55b396b3dde3754ad93784
Sam Escolas <samescolas@gmail.com>
2017-12-06 19:15:21 -0500

    Adding 50 -- Consecutive Sum Primes.

---
+++ 50/consecutive_primes.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+
+from math import sqrt
+
+MAX_VAL = 1_000_000
+
+def is_prime(n):
+	if n < 2 or (n % 2 == 0 and n > 2):
+		return False
+	ubound = int(sqrt(n)) + 1
+	for i in range(3, ubound, 2):
+		if n % i == 0:
+			return False
+	return True
+
+def find_max_sum(ix, primes):
+	if primes[ix] > MAX_VAL:
+		return 0,0
+	candidates = [primes[ix]]
+	while candidates[-1] < MAX_VAL:
+		candidates.append(candidates[-1] + primes[ix+len(candidates)])
+	prime_candidates = list(filter(lambda n: n < MAX_VAL and is_prime(n), candidates))
+	return candidates.index(prime_candidates[-1]) + 1, prime_candidates[-1]
+
+
+primes = list(filter(lambda x: is_prime(x), range(1, 100_000)))
+prime_prime_sums = []
+max_len = 0
+max_val = 0
+
+for start_ix in range(250):
+	tmp_max_len, tmp_max_val = find_max_sum(start_ix, primes)
+	if tmp_max_len > max_len:
+		max_val, max_len = tmp_max_val, tmp_max_len
+
+print("The maximum consecutive sum prime is {} with a length of {}.".format(max_val, max_len))
